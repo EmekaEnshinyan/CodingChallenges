f524a7b8a98431f4ad554dae4c5b83f841c288f2
Emeka Anekwe <59901971+EmekaEnshinyan@users.noreply.github.com>
2023-01-06 18:36:26 -0800

    Update population.sql

--- SQL/population.sql
+++ SQL/population.sql
@@ -1,9 +1,7 @@
-Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+/* Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 
-The CITY table is described as follows:
+The CITY table is described as follows: https://www.hackerrank.com/challenges/revising-the-select-query/problem
 
-
-
-Soltuion:
+Soltuion: */
 
 SELECT * FROM CITY WHERE population > 100000 AND Countrycode ="USA";
